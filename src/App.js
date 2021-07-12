4628e9c01d334f58efa6b3b534ba06bae8f163a6
jsun969 <i@jsun969.cn>
2021-07-12 19:09:40 +0800

    :lipstick: Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, AppBar, Toolbar, Typography } from '@material-ui/core';
+import PostButton from './components/PostButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">万能墙</Typography>
+        </Toolbar>
+      </AppBar>
+      <PostButton />
+    </Box>
   );
 }
 
---
+++ src/components/PostButton.js
@@ -0,0 +1,48 @@
+import { SpeedDial, SpeedDialIcon, SpeedDialAction } from '@material-ui/lab';
+import { Edit, Favorite, RecordVoiceOver, Help, Notifications, People } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  speedDial: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const actions = [
+  { icon: <Favorite />, name: '表白' },
+  { icon: <RecordVoiceOver />, name: '吐槽' },
+  { icon: <Help />, name: '求助' },
+  { icon: <Notifications />, name: '通知' },
+  { icon: <People />, name: '扩列' },
+];
+
+export default function PostButton() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleAction = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <SpeedDial
+        ariaLabel="Post Button"
+        className={classes.speedDial}
+        icon={<SpeedDialIcon openIcon={<Edit />} />}
+        onClose={() => {
+          setOpen(false);
+        }}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction key={action.name} icon={action.icon} tooltipTitle={action.name} tooltipOpen onClick={handleAction} />
+        ))}
+      </SpeedDial>
+    </div>
+  );
+}
