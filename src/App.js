99a9951699dc4ff47ee2d2c4a050237409e83b53
jsun969 <i@jsun969.cn>
2021-07-14 11:33:35 +0800

    :sparkles: Add tabs in index
    - Remove react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { useState } from 'react';
+import { AppBar, Toolbar, Typography, Box, Tab } from '@material-ui/core';
+import { TabPanel, TabContext, TabList } from '@material-ui/lab';
 import { QuestionAnswer } from '@material-ui/icons';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { pink, purple } from '@material-ui/core/colors';
@@ -13,10 +15,12 @@ const theme = createTheme({
 });
 
 function App() {
+  const [tabIndex, setTabIndex] = useState('all');
+
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider>
-        <header>
+        <TabContext value={tabIndex}>
           <AppBar>
             <Toolbar>
               <QuestionAnswer />
@@ -24,11 +28,31 @@ function App() {
                 <Typography variant="h6">{process.env.REACT_APP_SCHOOL}万能墙</Typography>
               </Box>
             </Toolbar>
+            <TabList
+              onChange={(event, value) => {
+                setTabIndex(value);
+              }}
+              variant="scrollable"
+              scrollButtons="auto"
+            >
+              <Tab label="全部" value="all" />
+              <Tab label="表白" value="love" />
+              <Tab label="吐槽" value="complaint" />
+              <Tab label="求助" value="help" />
+              <Tab label="通知" value="notice" />
+              <Tab label="扩列" value="expand" />
+            </TabList>
           </AppBar>
-        </header>
-        <main>
-          <PostButton />
-        </main>
+          <Box pt={12}>
+            <TabPanel value="all">all</TabPanel>
+            <TabPanel value="love">love</TabPanel>
+            <TabPanel value="complaint">complaint</TabPanel>
+            <TabPanel value="help">help</TabPanel>
+            <TabPanel value="notice">notice</TabPanel>
+            <TabPanel value="expand">expand</TabPanel>
+          </Box>
+        </TabContext>
+        <PostButton />
       </SnackbarProvider>
     </ThemeProvider>
   );
