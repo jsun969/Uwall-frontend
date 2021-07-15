b2e2f13cd7d4d58a1c9af56774331e982c3cf007
jsun969 <i@jsun969.cn>
2021-07-15 22:38:38 +0800

    :sparkles: Add comment request

--- src/apis/index.js
+++ src/apis/index.js
@@ -9,3 +9,5 @@ export const sendLove = (data) => service.post('/love', data);
 export const getAllMessages = () => service.get('/messages');
 
 export const sendLike = (id) => service.patch('/like', { id });
+
+export const sendComment = (data) => service.post('/comment', data);
--- src/components/CommentDialog.jsx
+++ src/components/CommentDialog.jsx
@@ -1,6 +1,31 @@
 import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import { useState } from 'react';
+import { sendComment } from '../apis';
+import { useSnackbar } from 'notistack';
 
 export default function CommentDialog(props) {
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSubmit = async () => {
+    try {
+      const { status } = await sendComment({
+        id: props.id,
+        name,
+        comment,
+      });
+      if (status === 201) {
+        enqueueSnackbar('评论成功 , 请等待审核通过', { variant: 'success' });
+        props.onClose();
+      }
+    } catch (error) {
+      enqueueSnackbar(`评论失败 ${error}`, { variant: 'error' });
+      props.onClose();
+    }
+  };
+
   return (
     <div>
       <Dialog
@@ -12,8 +37,26 @@ export default function CommentDialog(props) {
         <DialogTitle>添加评论</DialogTitle>
         <DialogContent>
           <DialogContentText>给 {props.from} 评论</DialogContentText>
-          <TextField autoFocus margin="dense" id="name" label="你的名字" fullWidth />
-          <TextField autoFocus margin="dense" id="name" label="说点什么~" fullWidth />
+          <TextField
+            margin="dense"
+            label="你的名字"
+            fullWidth
+            inputProps={{ maxLength: 15 }}
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <TextField
+            margin="dense"
+            label="说点什么~"
+            fullWidth
+            inputProps={{ maxLength: 50 }}
+            value={comment}
+            onChange={(event) => {
+              setComment(event.target.value);
+            }}
+          />
         </DialogContent>
         <DialogActions>
           <Button
@@ -24,12 +67,7 @@ export default function CommentDialog(props) {
           >
             取消
           </Button>
-          <Button
-            onClick={() => {
-              props.onClose();
-            }}
-            color="primary"
-          >
+          <Button onClick={handleSubmit} color="primary">
             发送
           </Button>
         </DialogActions>
