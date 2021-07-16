0ab2fb20c756931b9a73c8ac290c4484b65a6afc
jsun969 <i@jsun969.cn>
2021-07-16 19:17:38 +0800

    :sparkles: Add link to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import { AppBar, Toolbar, Typography, Box, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Box, Tab, IconButton, Menu, MenuItem } from '@material-ui/core';
 import { TabPanel, TabContext, TabList } from '@material-ui/lab';
-import { QuestionAnswer } from '@material-ui/icons';
+import { QuestionAnswer, GitHub } from '@material-ui/icons';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { pink, purple } from '@material-ui/core/colors';
 import PostButton from './components/PostButton';
@@ -18,9 +18,12 @@ const theme = createTheme({
 
 function App() {
   const [tabIndex, setTabIndex] = useState('all');
+  const [githubPos, setGithubPos] = useState(null);
+
   useEffect(() => {
     storage.initialize();
   }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider maxSnack={1}>
@@ -28,9 +31,42 @@ function App() {
           <AppBar>
             <Toolbar>
               <QuestionAnswer />
-              <Box ml={1}>
+              <Box ml={1} style={{ flexGrow: 1 }}>
                 <Typography variant="h6">{process.env.REACT_APP_SCHOOL}万能墙</Typography>
               </Box>
+              <IconButton
+                color="inherit"
+                onClick={(event) => {
+                  setGithubPos(event.currentTarget);
+                }}
+              >
+                <GitHub />
+              </IconButton>
+              <Menu
+                open={Boolean(githubPos)}
+                anchorEl={githubPos}
+                onClose={() => {
+                  setGithubPos(null);
+                }}
+                keepMounted
+              >
+                <MenuItem
+                  onClick={() => {
+                    window.open('https://github.com/jsun969/Uwall-frontend');
+                    setGithubPos(null);
+                  }}
+                >
+                  前端 React
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    window.open('https://github.com/jsun969/Uwall-backend');
+                    setGithubPos(null);
+                  }}
+                >
+                  后端 Golang
+                </MenuItem>
+              </Menu>
             </Toolbar>
             <TabList
               onChange={(event, value) => {
