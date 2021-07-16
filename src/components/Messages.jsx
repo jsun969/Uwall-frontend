ec795b1bf2ccee13a020f18a907da1c992ffd9cf
jsun969 <i@jsun969.cn>
2021-07-16 22:12:58 +0800

    :lipstick: Add loading animation when click loading button

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,29 +1,53 @@
 import { getMessages } from '../apis';
 import { useEffect, useState } from 'react';
 import MessageCard from './MessageCard';
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { pink } from '@material-ui/core/colors';
 import storage from '../storage';
+
+const useStyles = makeStyles((theme) => ({
+  buttonProgress: {
+    color: pink,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 export default function Messages(props) {
   const { type } = props;
   const [messages, setMessages] = useState([]);
   const [page, setPage] = useState(1);
   const [showLoad, setShowLoad] = useState(true);
+  const [Loading, setLoading] = useState(false);
+
+  const classes = useStyles();
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const { data } = await getMessages(type, 0);
       if (data.length < 10) setShowLoad(false);
       setMessages(data);
+      setLoading(false);
     })();
   }, [type]);
 
   const handleLoad = () => {
     (async () => {
+      setLoading(true);
       const { data } = await getMessages(type, page);
-      if (data.length < 10) setShowLoad(false);
       setMessages([...messages, ...data]);
+      if (data.length < 10) {
+        setLoading(false);
+        setShowLoad(false);
+        return;
+      }
       setPage(page + 1);
+      setLoading(false);
     })();
   };
 
@@ -51,7 +75,8 @@ export default function Messages(props) {
       ))}
       <Grid item container justifyContent="center">
         <Grid item>
-          <Button onClick={handleLoad} color="secondary" variant="contained" disabled={!showLoad}>
+          <Button onClick={handleLoad} color="secondary" variant="contained" disabled={!showLoad || Loading}>
+            {Loading && <CircularProgress size={24} className={classes.buttonProgress} />}
             {showLoad ? '加载更多' : '没了'}
           </Button>
         </Grid>
